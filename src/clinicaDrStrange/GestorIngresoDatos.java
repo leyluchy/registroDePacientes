7bc48bd9c9412069ea671d79926f3738628aea54
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-06-15 11:57:38 -0300

    Interfaz lista

--- src/clinicaDrStrange/GestorIngresoDatos.java
+++ src/clinicaDrStrange/GestorIngresoDatos.java
@@ -2,15 +2,15 @@ package clinicaDrStrange;
 
 public class GestorIngresoDatos {
 
-	public boolean IngresoDatosPaciente(){
+	public boolean IngresoDatosPaciente(String codPac, String nyAp){
 		return true;
 	}
 	
-	public boolean IngresosituacionPaciente() {
+	public boolean IngresosituacionPaciente(String codPac, String codMed, String diag) {
 		return true;
 	}
 	
-	public boolean IngresoDatosMedico() {
+	public boolean IngresoDatosMedico(String codMed, String nyAp, String spe) {
 		return true;
 	}
 }

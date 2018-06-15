e9bffef25647798e32b229eedb3bd969bfc636c7
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-06-15 12:42:57 -0300

    gestor iniciando

--- src/clinicaDrStrange/GestorBD.java
+++ src/clinicaDrStrange/GestorBD.java
@@ -1,6 +1,22 @@
 package clinicaDrStrange;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class GestorBD {
 
+	public static boolean ingresarPaciente(int codPac, String nyap) {
+		PrintWriter datopac;
+		try {
+			datopac = new PrintWriter(new FileWriter("./datopac.txt", true));
+			datopac.println(codPac+"|"+nyap);
+			datopac.close();
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
 	
+	//public static boolean ingresarDiagnostico()
 }

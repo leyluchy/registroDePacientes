a6226beb79d2122244b02f18762baa2ed71743fb
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-06-15 11:53:14 -0300

    Inicio del proyecto con clases(y con clase)

--- src/clinicaDrStrange/GestorInformes.java
+++ src/clinicaDrStrange/GestorInformes.java
@@ -2,4 +2,11 @@ package clinicaDrStrange;
 
 public class GestorInformes {
 
+	public boolean traerListaDePacientesPorMedico(String codMed, String nomMed, String[] pacientes) {
+		return true;
+	}
+	
+	public boolean traerEspecialidad(String codMed, String nomMed, String spe) {
+		return true;
+	}
 }
--- src/clinicaDrStrange/GestorIngresoDatos.java
+++ src/clinicaDrStrange/GestorIngresoDatos.java
@@ -2,4 +2,15 @@ package clinicaDrStrange;
 
 public class GestorIngresoDatos {
 
+	public boolean IngresoDatosPaciente(){
+		return true;
+	}
+	
+	public boolean IngresosituacionPaciente() {
+		return true;
+	}
+	
+	public boolean IngresoDatosMedico() {
+		return true;
+	}
 }
